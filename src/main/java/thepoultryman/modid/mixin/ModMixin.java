594639fd18e59846076a15fcd9530050f139a667
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-01-31 17:21:12 -0800

    Set up template

--- src/main/java/thepoultryman/modid/mixin/ModMixin.java
+++ src/main/java/thepoultryman/modid/mixin/ModMixin.java
@@ -1,16 +1,16 @@
-package net.fabricmc.example.mixin;
+package thepoultryman.modid.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import thepoultryman.modid.ModClass;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ModMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ModClass.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
